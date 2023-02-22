b3b94d9e893e6738785dbfd12011bf0c82a62943
Yifei Zhu <yz6994@nyu.edu>
2023-02-22 16:17:39 -0500

    saving my changes to sin4_vector

--- fast-sin copy.cpp
+++ fast-sin copy.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
 #include "utils.h"
@@ -23,9 +24,12 @@ static constexpr double c9  =  1/(((double)2)*3*4*5*6*7*8*9);
 static constexpr double c11 = -1/(((double)2)*3*4*5*6*7*8*9*10*11);
 // sin(x) = x + c3*x^3 + c5*x^5 + c7*x^7 + x9*x^9 + c11*x^11
 
+//static unsigned int seed = 10;
+
 void sin4_reference(double* sinx, const double* x) {
   for (long i = 0; i < 4; i++) sinx[i] = sin(x[i]);
 }
+
 
 void sin4_taylor(double* sinx, const double* x) {
   for (int i = 0; i < 4; i++) {
@@ -46,7 +50,7 @@ void sin4_taylor(double* sinx, const double* x) {
     sinx[i] = s;
   }
 }
-
+/*
 void sin4_intrin(double* sinx, const double* x) {
   // The definition of intrinsic functions can be found at:
   // https://software.intel.com/sites/landingpage/IntrinsicsGuide/#
@@ -75,19 +79,28 @@ void sin4_intrin(double* sinx, const double* x) {
   sin4_reference(sinx, x);
 #endif
 }
-
+*/
 void sin4_vector(double* sinx, const double* x) {
   // The Vec class is defined in the file intrin-wrapper.h
   typedef Vec<double,4> Vec4;
-  Vec4 x1, x2, x3;
+  Vec4 x1, x2, x3, x5, x7, x9, x11;
   x1  = Vec4::LoadAligned(x);
   x2  = x1 * x1;
   x3  = x1 * x2;
+  x5  = x3 * x2;
+  x7  = x5 * x2;
+  x9  = x7 * x2;
+  x11 = x9 * x2;
 
   Vec4 s = x1;
   s += x3  * c3 ;
+  s += x5  * c5 ;
+  s += x7  * c7 ;
+  s += x9  * c9 ;
+  s += x11 * c11;
   s.StoreAligned(sinx);
 }
+
 
 double err(double* x, double* y, long N) {
   double error = 0;
@@ -104,7 +117,8 @@ int main() {
   double* sinx_intrin = (double*) aligned_malloc(N*sizeof(double));
   double* sinx_vector = (double*) aligned_malloc(N*sizeof(double));
   for (long i = 0; i < N; i++) {
-    x[i] = (drand48()-0.5) * M_PI/2; // [-pi/4,pi/4]
+    x[i] = ( (rand() / (RAND_MAX + 1.0)) - 0.5) * M_PI/2; // [-pi/4,pi/4]
+    //std::cout << x[i] << std::endl;
     sinx_ref[i] = 0;
     sinx_taylor[i] = 0;
     sinx_intrin[i] = 0;
@@ -118,7 +132,7 @@ int main() {
     }
   }
   printf("Reference time: %6.4f\n", tt.toc());
-
+  
   tt.tic();
   for (long rep = 0; rep < 1000; rep++) {
     for (long i = 0; i < N; i+=4) {
@@ -127,6 +141,8 @@ int main() {
   }
   printf("Taylor time:    %6.4f      Error: %e\n", tt.toc(), err(sinx_ref, sinx_taylor, N));
 
+  /*
+
   tt.tic();
   for (long rep = 0; rep < 1000; rep++) {
     for (long i = 0; i < N; i+=4) {
@@ -134,6 +150,8 @@ int main() {
     }
   }
   printf("Intrin time:    %6.4f      Error: %e\n", tt.toc(), err(sinx_ref, sinx_intrin, N));
+  
+  */
 
   tt.tic();
   for (long rep = 0; rep < 1000; rep++) {
