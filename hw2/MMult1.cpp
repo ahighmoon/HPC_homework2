ad781cb23070dc54b3af94fa07b78cbda3a133fc
Yifei Zhu <yz6994@nyu.edu>
2023-03-05 22:21:27 -0500

    update

--- hw2/MMult1.cpp
+++ hw2/MMult1.cpp
@@ -26,46 +26,97 @@ void MMult0(long m, long n, long k, double *a, double *b, double *c) {
 
 void MMult1(long m, long n, long k, double *a, double *b, double *c) {
   // TODO: See instructions below
-    for (long ii = 0; ii < m; ii += BLOCK_SIZE)
-    {
-        for (long kk = 0; kk < k; kk += BLOCK_SIZE)
-        {
-            for (long j=0; j < n; j += 2)
-            {
-                for (long i = ii; i < ii + BLOCK_SIZE; i += 2)
-                {
-                    double acc00;
-                    double acc01;
-                    double acc10;
-                    double acc11;
-                    if (kk == 0){
-                        acc00 = 0;
-                        acc01 = 0;
-                        acc10 = 0;
-                        acc11 = 0;
-                    }
-                    else
-                    {
-                        acc00 = c[i + 0][j + 0];
-                        acc01 = c[i + 0][j + 1];
-                        acc10 = c[i + 1][j + 0];
-                        acc11 = c[i + 1][j + 1];
-                    }
-                    for (long kkk = kk; kkk < kk + BLOCK_SIZE; kkk++)
-                    {
-                        acc00 += b[kkk][j + 0] * a[i + 0][kkk];
-                        acc01 += b[kkk][j + 1] * a[i + 0][kkk];
-                        acc10 += b[kkk][j + 0] * a[i + 1][kkk];
-                        acc11 += b[kkk][j + 1] * a[i + 1][kkk];
-                    }
-                    c[i + 0][j + 0] = acc00;
-                    c[i + 0][j + 1] = acc01;
-                    c[i + 1][j + 0] = acc10;
-                    c[i + 1][j + 1] = acc11;
-                }
-            }
+  /*
+  for (long ii = 0; ii < m; ii += BLOCK_SIZE){
+    for (long kk = 0; kk < k; kk += BLOCK_SIZE){
+      for (long j=0; j < n; j += 2){
+        for (long i = ii; i < ii + BLOCK_SIZE; i += 2){
+          double acc00;
+          double acc01;
+          double acc10;
+          double acc11;
+          if (kk == 0){
+              acc00 = 0;
+              acc01 = 0;
+              acc10 = 0;
+              acc11 = 0;
+          }
+          else
+          {
+              acc00 = c[i + 0][j + 0];
+              acc01 = c[i + 0][j + 1];
+              acc10 = c[i + 1][j + 0];
+              acc11 = c[i + 1][j + 1];
+          }
+          for (long kkk = kk; kkk < kk + BLOCK_SIZE; kkk++)
+          {
+              acc00 += b[kkk][j + 0] * a[i + 0][kkk];
+              acc01 += b[kkk][j + 1] * a[i + 0][kkk];
+              acc10 += b[kkk][j + 0] * a[i + 1][kkk];
+              acc11 += b[kkk][j + 1] * a[i + 1][kkk];
+          }
+          c[i + 0][j + 0] = acc00;
+          c[i + 0][j + 1] = acc01;
+          c[i + 1][j + 0] = acc10;
+          c[i + 1][j + 1] = acc11;
+              }
+          }
+      }
+  }
+  */
+
+  {
+  double* A = (double*) aligned_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+  double* B = (double*) aligned_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+  double* C = (double*) aligned_malloc(BLOCK_SIZE * BLOCK_SIZE * sizeof(double));
+  
+  for (long i = 0; i < BLOCK_SIZE*BLOCK_SIZE; i++) {
+    A[i] = 0.0;
+    B[i] = 0.0;
+    C[i] = 0.0;
+  }
+  long M = m / BLOCK_SIZE;
+  long N = n / BLOCK_SIZE;
+  long K = k / BLOCK_SIZE;
+
+  // #pragma omp for
+  for (long BLOCK = 0; BLOCK < M*N; BLOCK++) {
+    int i = BLOCK % M;
+    int j = BLOCK / M;
+
+      for (int ic = 0; ic < BLOCK_SIZE; ic++) {
+        for (int jc = 0; jc < BLOCK_SIZE; jc++) {
+          C[jc*BLOCK_SIZE + ic] = c[m*(j*BLOCK_SIZE+jc) + i*BLOCK_SIZE + ic];
         }
+      }
+
+      //read matrix A, B
+      for (long p = 0; p < K; p++) {
+        for (int ia = 0; ia < BLOCK_SIZE; ia++) {
+          for (int ja = 0; ja < BLOCK_SIZE; ja++) {
+            A[ja*BLOCK_SIZE + ia] = a[m*(p*BLOCK_SIZE+ja) + i*BLOCK_SIZE + ia];
+          }
+        }
+        for (int ib = 0; ib < BLOCK_SIZE; ib++) {
+          for (int jb = 0; jb < BLOCK_SIZE; jb++) {
+            B[jb*BLOCK_SIZE + ib] = b[k*(j*BLOCK_SIZE+jb) + p*BLOCK_SIZE + ib];
+          }
+        }
+        MMult0(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, A, B, C);
+      }
+
+      //C back to c
+      for (int ic = 0; ic < BLOCK_SIZE; ic++) {
+        for (int jc = 0; jc < BLOCK_SIZE; jc++) {
+          c[m*(j*BLOCK_SIZE + jc) + i*BLOCK_SIZE + ic] = C[jc*BLOCK_SIZE + ic];
+        }
+      }
     }
+
+  aligned_free(A);
+  aligned_free(B);
+  aligned_free(C);
+}
 }
 
 int main(int argc, char** argv) {
